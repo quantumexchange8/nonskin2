f185423856829699d40dec0fc0350ff318204c40
Irfan Zulkarnain <irfanzulkarnain.dev@yahoo.com>
2023-07-17 12:56:51 +0800

    update

--- resources/views/admin/products/list.blade.php
+++ resources/views/admin/products/list.blade.php
@@ -20,21 +20,15 @@
 @include('admin.products.modal-update-product')
 
 <div class="col-xl-12 col-lg-8">
-
-    <div class="tab-content p-3 text-muted">
-        <div class="tab-pane active" id="popularity" role="tabpanel">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <button type="button" class="btn btn-primary waves-effect waves-light"
-                                data-bs-toggle="modal" data-bs-target=".modal-update-product"><i
-                                    class='bx bx-plus-circle'></i> Add Product</button>
-                        </div>
-                        <div class="card-body">
-                            <div id="table-product-list"></div>
-                        </div>
-                    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <button type="button" class="btn btn-primary waves-effect waves-light" data-bs-toggle="modal"
+                        data-bs-target=".modal-update-product"><i class='bx bx-plus-circle'></i> Add Product</button>
+                </div>
+                <div class="card-body">
+                    <div id="table-product-list"></div>
                 </div>
             </div>
         </div>
@@ -48,32 +42,37 @@
 <script src="{{ URL::asset('assets/js/app.js') }}"></script>
 
 <script>
-    var products = {!! $products->map(function($product) {
-        $formattedPrice = number_format($product->price, 2);
-        $formattedPriceWithCurrency = 'RM ' . $formattedPrice;
-        $actionButton = '';
-        return [
-            $product->code,
-            $product->name_en,
-            $product->category->name_en,
-            $product->shipping_quantity,
-            $formattedPriceWithCurrency,
-            $product->image,
-            $product->status,
-            $actionButton,
-        ];
-    })->toJson() !!};
+    var products = {!! $products->map(function ($product) {
+            $formattedPrice = number_format($product->price, 2);
+            $formattedPriceWithCurrency = 'RM ' . $formattedPrice;
+
+            return [
+                $product->code,
+                $product->name_en,
+                $product->category->name_en,
+                $product->shipping_quantity,
+                $formattedPriceWithCurrency,
+                $product->image,
+                $product->status,
+            ];
+        })->toJson() !!};
 
     (function() {
         var __webpack_exports__ = {};
 
         // Basic Table
         new gridjs.Grid({
-            columns: ["Product Code", "Product Name", "Category", "Shipping Quantity", "Price", "Image",
-                "Status", "Action"
+            columns: ["Product Code",
+                "Product Name",
+                "Category",
+                "Shipping Quantity",
+                "Price",
+                "Image",
+                "Status",
+                "Action"
             ],
             pagination: {
-                limit: 5
+                limit: 8
             },
             sort: true,
             search: true,
@@ -81,7 +80,4 @@
         }).render(document.getElementById("table-product-list"));
     })();
 </script>
-
-
-
 @endsection
