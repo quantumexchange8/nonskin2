3f784fd9e3d99d9fbb11d1325998c83daf80da84
Irfan Zulkarnain <irfanzulkarnain.dev@yahoo.com>
2023-07-28 11:11:16 +0800

    update route

--- routes/admin.php
+++ routes/admin.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\OrderController;
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.',  'middleware' => 'auth'], function () {
     Route::get('/member-list', [UserController::class, 'memberList'])->name('member-list');
-    Route::get('/list', [ProductController::class, 'list'])->name('product-list');
+    Route::get('/product-list', [ProductController::class, 'list'])->name('product-list');
     Route::get('/new-orders', [OrderController::class, 'new'])->name('new-order-list');
     Route::get('/order-history', [OrderController::class, 'history'])->name('order-history-list');
 
